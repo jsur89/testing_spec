589a1f38424914c100ef46819c551962c8f48201
Jared Suresky <jsuresky@gmail.com>
2020-12-07 21:43:16 -0500

    jared commits

--- test/testing.spec.js
+++ test/testing.spec.js
@@ -1,16 +1,29 @@
-const { expect } = require('chai');
-const wrap = require('../wrap');
+const { expect } = require("chai");
+const wrap = require("../wrap");
+const twoSum = require("../twoSum");
 
-describe('wrap', () => {
-  it('Returns empty string if empty string was provided', () => {
-    expect(wrap('', 10)).to.equal('');
+describe("twoSum", () => {
+  it("If array length is less than 2, return an empty array", () => {
+    expect(twoSum([4], 1)).to.eql([]);
   });
-  it('cannot be undefined', () => {
-    expect(wrap('This is a test', 10)).to.not.equal(undefined);
+});
+
+/*
+(1) If less than 2, return empty arr
+(2) If there's no possible solution ....return empty arr
+(3) The indexes cannot be the same index
+*/
+
+describe("wrap", () => {
+  xit("Returns empty string if empty string was provided", () => {
+    expect(wrap("", 10)).to.equal("");
   });
-  it('less than 10', () => {
-    const response = wrap('This is a longer test', 10);
-    const arr = response.split('\n');
+  xit("cannot be undefined", () => {
+    expect(wrap("This is a test", 10)).to.not.equal(undefined);
+  });
+  xit("less than 10", () => {
+    const response = wrap("This is a longer test", 10);
+    const arr = response.split("\n");
     const lengthGreaterThan10 = arr.filter((item) => item.length > 10).length;
     expect(lengthGreaterThan10).to.equal(0);
     //console.log(lengthGreaterThan10);
